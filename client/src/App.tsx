dcee99a1aa88c7b7a3e549ab8623cffb0e2362da
Theo Freydiere <tfreydie@z4r6p1.42lyon.fr>
2026-05-15 18:26:24 +0200

    client fixed and now using typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+type TestType = {
+  age: number;
+  name: string;
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [test, setTest] = useState<TestType>({age: 30, name: "Theo"});
+
+  return (
+    <>
+     <div className="size-48 bg-amber-500">
+        <ul className="size-10"> welcome to my tetris game, I am {test.name} and I am {test.age} years old</ul>
+     </div>
+    </>
+  )
+}
+
+export default App
