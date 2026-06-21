db2834ee71bdcb275600b1a92463ee2f1fceb026
ptitdrogo <moitheo2@hotmail.fr>
2026-06-21 17:46:46 +0200

    fix: Block lines spawnig now works properly
    Two issues were fixed:
    - cleared lines not resetting to 0
    - Block lines now works on all down input and not just the periodic down input of the gameloop.

--- server/src/game/Game.ts
+++ server/src/game/Game.ts
@@ -1,191 +1,192 @@
 import {
-    GameInput,
-    GameOverData,
-    GameOverRanking,
-    GameState,
-    GameStatus,
-    Room,
-    ServerMessage,
+  GameInput,
+  GameOverData,
+  GameOverRanking,
+  GameState,
+  GameStatus,
+  Room,
+  ServerMessage,
 } from "../../../shared/types.js";
 import { Player, SPEED_STEP, STARTING_SPEED } from "./Player.js";
 import { Server } from "socket.io";
 import { gameService } from "../services/GameService.js";
 import { roomManager, RoomManager } from "../services/RoomManager.js";
+import { Board } from "./Board.js";
 
 const UPDATE_DELAY_MS = 100;
 const META_UPDATE_DELAY_MS = 1000;
 export class Game {
-    private players: Player[];
-    private io: Server;
-    private roomId: string;
-    private gameLoop: NodeJS.Timeout | undefined;
-    private metaLoop: NodeJS.Timeout | undefined;
+  private players: Player[];
+  private io: Server;
+  private roomId: string;
+  private gameLoop: NodeJS.Timeout | undefined;
+  private metaLoop: NodeJS.Timeout | undefined;
 
-    constructor(players: Player[], io: Server, roomId: string) {
-        this.players = players;
-        this.io = io;
-        this.roomId = roomId;
+  constructor(players: Player[], io: Server, roomId: string) {
+    this.players = players;
+    this.io = io;
+    this.roomId = roomId;
+  }
+
+  getPlayers() {
+    return this.players;
+  }
+
+  stopGame() {
+    if (!this.gameLoop) return;
+    clearInterval(this.gameLoop);
+    this.gameLoop = undefined;
+
+    if (!this.metaLoop) return;
+    clearInterval(this.metaLoop);
+    this.metaLoop = undefined;
+
+    gameService.removeGame(this);
+    //Room le statut de redevenir waiting dans le room
+    if (this.players.length === 0) return;
+    const room = roomManager.getRoomBySocketId(this.players[0].getSocketId());
+    if (!room) return;
+    room.gameInfo.status = GameStatus.WAITING;
+  }
+
+  private sendDataToPlayers() {
+    const playersData = this.players.map((player) => {
+      return {
+        name: player.getName(),
+        score: player.getPoints(),
+        board: player.getBoard().getFullGrid(),
+        isAlive: player.getBoard().getIsAlive(),
+        level: Math.floor(player.getPoints() / 500),
+      };
+    });
+    const gameUpdate: GameState = {
+      players: playersData,
+    };
+    this.io.to(this.roomId).emit(ServerMessage.GAME_STATE, gameUpdate);
+  }
+
+  handleGameInput(newInput: GameInput, socketId: string) {
+    const currTime = Date.now();
+    this.players = this.players.map((player) =>
+      player.getSocketId() === socketId
+        ? Player.handleInput(player, newInput, currTime)
+        : player
+    );
+    this.players = Game.handleClearedLines(this.players);
+    this.sendDataToPlayers();
+  }
+
+  killPlayer(socketId: string) {
+    this.players = this.players.map((p) =>
+      p.getSocketId() === socketId ? Player.killPlayer(p) : p
+    );
+  }
+
+  static createGame(players: Player[], io: Server, room: Room) {
+    const newGame = new Game(players, io, room.id);
+    gameService.addGame(newGame);
+    return newGame;
+  }
+
+  start() {
+    if (this.players.length === 0) {
+      throw new Error("Cannot start a game with no players");
     }
 
-    getPlayers() {
-        return this.players;
-    }
+    this.sendDataToPlayers();
 
-    stopGame() {
-        if (!this.gameLoop) return;
-        clearInterval(this.gameLoop);
-        this.gameLoop = undefined;
+    this.players = this.players.map((player) =>
+      Player.copy(player, { lastDowntime: Date.now() })
+    );
 
-        if (!this.metaLoop) return;
-        clearInterval(this.metaLoop);
-        this.metaLoop = undefined;
+    this.gameLoop = setInterval(() => {
+      const currTime = Date.now();
+      let hasMovedDown = false;
 
-        gameService.removeGame(this);
-        //Room le statut de redevenir waiting dans le room
-        if (this.players.length === 0) return;
-        const room = roomManager.getRoomBySocketId(
-            this.players[0].getSocketId(),
-        );
-        if (!room) return;
-        room.gameInfo.status = GameStatus.WAITING;
-    }
-
-    private sendDataToPlayers() {
-        const playersData = this.players.map((player) => {
-            return {
-                name: player.getName(),
-                score: player.getPoints(),
-                board: player.getBoard().getFullGrid(),
-                isAlive: player.getBoard().getIsAlive(),
-                level: Math.floor(player.getPoints() / 500),
-            };
-        });
-        const gameUpdate: GameState = {
-            players: playersData,
-        };
-        this.io.to(this.roomId).emit(ServerMessage.GAME_STATE, gameUpdate);
-    }
-
-    handleGameInput(newInput: GameInput, socketId: string) {
-        const currTime = Date.now();
-        this.players = this.players.map((player) =>
-            player.getSocketId() === socketId
-                ? Player.handleInput(player, newInput, currTime)
-                : player,
-        );
-
-        this.sendDataToPlayers();
-    }
-
-    killPlayer(socketId: string) {
-        this.players = this.players.map((p) =>
-            p.getSocketId() === socketId ? Player.killPlayer(p) : p,
-        );
-    }
-
-    static createGame(players: Player[], io: Server, room: Room) {
-        const newGame = new Game(players, io, room.id);
-        gameService.addGame(newGame);
-        return newGame;
-    }
-
-    start() {
-        if (this.players.length === 0) {
-            throw new Error("Cannot start a game with no players");
+      this.players = this.players.map((player) => {
+        if (currTime - player.getLastDownTime() > player.getSpeed()) {
+          hasMovedDown = true;
+          return Player.handleInput(player, GameInput.DOWN, currTime);
         }
+        return player;
+      });
 
+      if (hasMovedDown) {
         this.sendDataToPlayers();
+      }
+      
+    }, UPDATE_DELAY_MS);
 
-        this.players = this.players.map((player) =>
-            Player.copy(player, { lastDowntime: Date.now() }),
+    this.metaLoop = setInterval(() => {
+      if (this.players.length === 1) {
+        if (this.players[0].getBoard().getIsAlive()) return;
+        const playerDAta: GameOverData = {
+          ranking: [
+            {
+              name: this.players[0].getName(),
+              points: this.players[0].getPoints(),
+            },
+          ],
+        };
+        this.io.to(this.roomId).emit(ServerMessage.GAME_OVER, playerDAta);
+        this.stopGame();
+        return;
+      }
+
+      const alivePlayers = this.players.filter((player) =>
+        player.getBoard().getIsAlive()
+      );
+
+      if (alivePlayers.length === 1) {
+        //We send a message on a new subscriptions, GAME_OVER
+        const winner = alivePlayers[0];
+        const playersData: GameOverData = {
+          ranking: Game.getRanking(this.players),
+        };
+
+        this.io.to(this.roomId).emit(ServerMessage.GAME_OVER, playersData);
+        this.stopGame();
+        this.io.emit(
+          ServerMessage.LOBBY_STATE,
+          roomManager.getAvailableRooms()
         );
+      }
+    }, META_UPDATE_DELAY_MS);
+  }
 
-        this.gameLoop = setInterval(() => {
-            const currTime = Date.now();
-            let didUpdate = false;
+  private static getRanking(players: Player[]): GameOverRanking[] {
+    const ranking: GameOverRanking[] = players.map((p) => {
+      return { name: p.getName(), points: p.getPoints() };
+    });
 
-            this.players = this.players.map((player) => {
-                if (currTime - player.getLastDownTime() > player.getSpeed()) {
-                    didUpdate = true;
-                    return Player.handleInput(player, GameInput.DOWN, currTime);
-                }
-                return player;
-            });
+    return ranking.sort((a, b) => b.points - a.points);
+  }
 
-            if (didUpdate) {
-                this.players = Game.handleClearedLines(this.players);
-                this.sendDataToPlayers();
-            }
-        }, UPDATE_DELAY_MS);
-
-        this.metaLoop = setInterval(() => {
-            if (this.players.length === 1) {
-                if (this.players[0].getBoard().getIsAlive()) return;
-                const playerDAta: GameOverData = {
-                    ranking: [
-                        {
-                            name: this.players[0].getName(),
-                            points: this.players[0].getPoints(),
-                        },
-                    ],
-                };
-                this.io
-                    .to(this.roomId)
-                    .emit(ServerMessage.GAME_OVER, playerDAta);
-                this.stopGame();
-                // this.io
-                //     .to(this.roomId)
-                //     .emit(ServerMessage.GAME_OVER, playerDAta);
-                return;
-            }
-
-            const alivePlayers = this.players.filter((player) =>
-                player.getBoard().getIsAlive(),
-            );
-
-            if (alivePlayers.length === 1) {
-                //We send a message on a new subscriptions, GAME_OVER
-                const winner = alivePlayers[0];
-                const playersData: GameOverData = {
-                    ranking: Game.getRanking(this.players),
-                };
-
-                this.io
-                    .to(this.roomId)
-                    .emit(ServerMessage.GAME_OVER, playersData);
-                this.stopGame();
-                this.io.emit(
-                    ServerMessage.LOBBY_STATE,
-                    roomManager.getAvailableRooms(),
-                );
-            }
-        }, META_UPDATE_DELAY_MS);
+  private static handleClearedLines(players: Player[]): Player[] {
+    if (players.length === 1) {
+      return players;
     }
+    let to_add: number[] = Array(players.length).fill(0);
 
-    private static getRanking(players: Player[]): GameOverRanking[] {
-        const ranking: GameOverRanking[] = players.map((p) => {
-            return { name: p.getName(), points: p.getPoints() };
+    players.forEach((player, index) => {
+      const linesCleared = player.getBoard().getClearedLines();
+      if (linesCleared) {
+        to_add = to_add.map((n, i) => {
+          return i === index ? n : n + linesCleared;
         });
+      }
+    });
 
-        return ranking.sort((a, b) => b.points - a.points);
-    }
+    if (to_add.every((n) => n === 0)) return players;
 
-    private static handleClearedLines(players: Player[]): Player[] {
-        let to_add: number[] = Array(players.length).fill(0);
-
-        players.forEach((player, index) => {
-            const linesCleared = player.getBoard().getClearedLines();
-            if (linesCleared) {
-                to_add = to_add.map((n, i) => {
-                    return i === index ? n : n + linesCleared;
-                });
-            }
+    const newPlayers: Player[] = players
+      .map((p, i) => Player.addBlockLines(to_add[i], p))
+      .map((p) => {
+        return Player.copy(p, {
+          board: Board.copy(p.getBoard(), { clearedLines: 0 }),
         });
+      });
 
-        const newPlayers: Player[] = players.map((p, i) =>
-            Player.addBlockLines(to_add[i], p),
-        );
-
-        return newPlayers;
-    }
+    return newPlayers;
+  }
 }
