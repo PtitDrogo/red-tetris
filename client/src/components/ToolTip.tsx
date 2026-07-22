86a0b0cb3e3e09fa80c5fe5c8321fb2a568b1e06
GabrielRivo <gabriel.rivo95@gmail.com>
2026-07-22 21:37:33 +0200

    fixed tooltip not displaying

--- client/src/components/ToolTip.tsx
+++ client/src/components/ToolTip.tsx
@@ -6,7 +6,7 @@ type InfoTooltipProps = {
 
 export function InfoTooltip({ message }: InfoTooltipProps) {
     return (
-        <span className="relative inline-flex group ml-1 align-middle">
+        <span className="relative inline-flex group ml-1 align-middle z-51">
             <span className="flex items-center justify-center w-4 h-4 rounded-full bg-gray-500 text-white text-[10px] leading-none cursor-help select-none">
                 <BadgeInfo />
             </span>
